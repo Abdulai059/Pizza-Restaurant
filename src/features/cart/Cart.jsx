26554a0160933773e8726c7857d2a3ba74c09a1a
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-22 21:29:07 +0000

    update on styling

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import LinkButton from "@/ui/LinkButton";
+import Button from "@/ui/Button";
+
 
 // const fakeCart = [
 //   {
@@ -30,14 +32,13 @@ function Cart() {
 
   return (
     <div>
-      <LinkButton>
-        &larr; Back to menu
-      </LinkButton>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Button to="/order/new">Order pizzas</Button>
+
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,13 @@
-function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to }) {
+  const className =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 hover:ring hover:ring-yellow-300 hover:ring-offset-2 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to) return <Link to={to} className={className}>{children}</Link>;
+
   return (
-    <button
-      disable={disabled}
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 hover:ring hover:ring-yellow-300 hover:ring-offset-2 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4"
-    >
+    <button disable={disabled} className={className}>
       {children}
     </button>
   );
