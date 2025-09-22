a2e2842ea97a90c3cd5465125db92b3760636efe
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-22 22:02:26 +0000

    update on styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 flex items-center justify-between">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest uppercase md:text-3xl">
         Fast React Pizza Co.
       </Link>
