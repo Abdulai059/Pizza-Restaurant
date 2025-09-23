41700f2c3625b5a83a1fcc1910a38c11e78388c8
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-23 17:38:24 +0000

    update on styling

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -7,8 +7,6 @@ import {
   formatDate,
 } from "../../utils/helpers";
 
-
-
 function Order() {
   const order = useLoaderData();
 
@@ -27,7 +25,7 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
+    <div className="px-4 py-6">
       <div>
         <h2>Status</h2>
 
