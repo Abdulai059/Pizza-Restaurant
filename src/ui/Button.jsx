5d569c998c6d95130173fa82b0befa33dc614ab9
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-24 16:54:51 +0000

    initial update redux

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, variant = "primary", ...props }) {
+function Button({
+  children,
+  disabled,
+  to,
+  variant = "primary",
+  onClick,
+  ...props
+}) {
+  onabort;
   const base =
     "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
@@ -18,11 +26,17 @@ function Button({ children, disabled, to, variant = "primary", ...props }) {
       </Link>
     );
 
-  return (
-    <button disabled={disabled} className={styles[variant]} {...props}>
-      {children}
-    </button>
-  );
+  if (onClick)
+    return (
+      <button
+        onClick={onClick} 
+        disabled={disabled}
+        className={styles[variant]}
+        {...props}
+      >
+        {children}
+      </button>
+    );
 }
 
 export default Button;
