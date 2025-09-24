2fa3bf6172dd62beb0063e66cbc845fb3bca77f4
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-24 08:07:07 +0000

    initial update redux

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -6,6 +6,7 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 function Order() {
   const order = useLoaderData();
@@ -19,7 +20,7 @@ function Order() {
     priorityPrice,
     orderPrice,
     estimatedDelivery,
-    // cart,
+    cart,
   } = order;
 
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
@@ -41,19 +42,31 @@ function Order() {
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 py-5 px-6">
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
         <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
-        <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
+      <ul className="divide-y divide-stone-200 border-t border-b">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.key} />
+        ))}
+      </ul>
+
       <div className="space-y-2 bg-stone-200 px-6 py-5">
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
         {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
